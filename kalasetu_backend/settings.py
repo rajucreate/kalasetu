8f270fcf326602e3bcf03523b6f2a37fcef116c3
rajucreate <svaradaraju8@gmail.com>
2026-04-29 12:57:00 +0530

    added cord

--- kalasetu_backend/settings.py
+++ kalasetu_backend/settings.py
@@ -242,6 +242,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5174',
+    'https://kalasetu-5u0s.onrender.com',
 ]
 
 extra_cors_origins = [
